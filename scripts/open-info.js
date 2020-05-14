fde9078917407ef30791b7b07c467708ce8909ec
llllQ <matthew.penlington@gmail.com>
2020-05-14 15:52:58 +0100

    Major UI Updates
    -Moved to SCSS
    
    -Added favicon
    
    -Removed SVGs that were never being used
    
    -Removed info panel in current implementation form (added to temp.html, commented out CSS)
    
    -Added navigation menu for mobile devices as per Xd design spec
    
    #TODO media queries to remove navigation menu for desktop replace with info button
    
    #TODO add frosted glass panels for each nav menu item

--- scripts/open-info.js
+++ scripts/open-info.js
@@ -1,10 +1,23 @@
-const openInfo = () => {
-  const infoIcon = document.querySelector(".info-icon");
-  const infoContent = document.querySelector(".info-container");
+// const openInfo = () => {
+//   const infoIcon = document.querySelector(".info-icon");
+//   const infoContent = document.querySelector(".info-container");
 
-  infoIcon.addEventListener("click", () => {
-    infoIcon.classList.toggle("closed");
+//   infoIcon.addEventListener("click", () => {
+//     infoIcon.classList.toggle("closed");
+//   });
+// };
+
+// openInfo();
+
+const openNav = () => {
+  const navIcons = document.querySelectorAll(".nav-icon");
+  const nav = document.querySelector(".navigation");
+
+  navIcons.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      nav.classList.toggle("open");
+    });
   });
 };
 
-openInfo();
+openNav();
