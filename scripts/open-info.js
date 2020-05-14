1bc23408d7b7003bcada65c900666a7756bb2052
llllQ <matthew.penlington@gmail.com>
2020-05-14 21:43:17 +0100

    Frosted Panel
    Added frosted panel which shall be used for mobile 'info' and 'create' navigation actions
    
    TODO:
    
    -add media queries to change frosted panel action on desktops
    
    -add media queries to make frosted panel look good across mobile devices
    
    -add 'create' star form

--- scripts/open-info.js
+++ scripts/open-info.js
@@ -12,11 +12,46 @@
 const openNav = () => {
   const navIcons = document.querySelectorAll(".nav-icon");
   const nav = document.querySelector(".navigation");
+  const createBtn = document.querySelector(".create");
+  const viewBtn = document.querySelector(".view");
+  const infoBtn = document.querySelector(".info");
+  const infoRtn = document.querySelector(".info-return");
+  const createRtn = document.querySelector(".create-return");
+  const viewRtn = document.querySelector(".view-return");
+  const frostedPanel = document.querySelector(".frosted-panel");
 
   navIcons.forEach((icon) => {
     icon.addEventListener("click", () => {
       nav.classList.toggle("open");
     });
+  });
+
+  infoBtn.addEventListener("click", () => {
+    frostedPanel.classList.toggle("open");
+    frostedPanel.classList.add("info");
+  });
+
+  createBtn.addEventListener("click", () => {
+    frostedPanel.classList.toggle("open");
+    frostedPanel.classList.add("create");
+  });
+
+  viewBtn.addEventListener("click", () => {
+    frostedPanel.classList.toggle("open");
+    frostedPanel.classList.add("view");
+  });
+
+  infoRtn.addEventListener("click", () => {
+    frostedPanel.classList.remove("info");
+    frostedPanel.classList.toggle("open");
+  });
+  createRtn.addEventListener("click", () => {
+    frostedPanel.classList.remove("create");
+    frostedPanel.classList.toggle("open");
+  });
+  viewRtn.addEventListener("click", () => {
+    frostedPanel.classList.remove("view");
+    frostedPanel.classList.toggle("open");
   });
 };
 
